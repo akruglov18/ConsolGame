b8a38ea915ee0506a0e057245799bae704d29459
akruglov18 <a.kruglov.2001@mail.ru>
2022-02-28 21:17:42 +0300

    moving files

--- fix_format_all.sh
+++ fix_format_all.sh
@@ -3,7 +3,7 @@
 THIS_PATH="$(realpath "$0")"
 THIS_DIR="$(dirname "$THIS_PATH")"
 
-for dir in "armor_set" "creature" "drawer" "field" "game" "items" "main" "resource_holder" "screen" "skills_graph" "tests" "tiles" "utils" "weapons"
+for dir in "action" "creature" "equipment" "field" "game" "interfaces" "main" "resource_holder" "skills_graph" "tests" "utils"
 do
     FILE_LIST="$(find "$THIS_DIR/$dir"| grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
     echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
